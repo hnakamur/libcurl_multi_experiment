8d76d198cc3ae56bef7adcc0d97a9a241456cb6b
Hiroaki Nakamura <hnakamur@gmail.com>
2023-12-28 19:00:00 +0900

    Send request body with POST method

--- src/main.c
+++ src/main.c
@@ -41,13 +41,18 @@
  * Download an HTTP file and upload an FTP file simultaneously.
  */
 
-#define HANDLECOUNT 80 /* Number of simultaneous transfers */
+#define REQBODYSIZE 1048576 /* Request body size (=1MiB) */
+#define HANDLECOUNT 511     /* Number of simultaneous transfers */
 
 int
 main(void)
 {
+  static char req_body[REQBODYSIZE + 1];
   CURL *handles[HANDLECOUNT];
   CURLM *multi_handle;
+
+  memset(req_body, 'a', sizeof(req_body) - 1);
+  req_body[sizeof(req_body) - 1] = '\0';
 
   int still_running = 1; /* keep number of running handles */
   int i;
@@ -55,28 +60,44 @@ main(void)
   CURLMsg *msg;  /* for picking up messages with the transfer status */
   int msgs_left; /* how many messages are left */
 
+  CURLcode res;
+
   /* Allocate one CURL handle per transfer */
   for (i = 0; i < HANDLECOUNT; i++) {
     handles[i] = curl_easy_init();
-    curl_easy_setopt(handles[i], CURLOPT_URL, "http://localhost");
+    res        = curl_easy_setopt(handles[i], CURLOPT_URL, "http://localhost/limit-mem");
+    if (res != CURLE_OK) {
+      fprintf(stderr, "cannot set url: %s", curl_easy_strerror(res));
+    }
+    res = curl_easy_setopt(handles[i], CURLOPT_POSTFIELDS, req_body);
+    if (res != CURLE_OK) {
+      fprintf(stderr, "cannot set request body: %s", curl_easy_strerror(res));
+    }
   }
 
   /* init a multi stack */
   multi_handle = curl_multi_init();
 
   /* add the individual transfers */
-  for (i = 0; i < HANDLECOUNT; i++)
-    curl_multi_add_handle(multi_handle, handles[i]);
+  CURLMcode mc;
+  for (i = 0; i < HANDLECOUNT; i++) {
+    mc = curl_multi_add_handle(multi_handle, handles[i]);
+    if (mc != CURLM_OK) {
+      fprintf(stderr, "cannot add handle: %s", curl_multi_strerror(mc));
+    }
+  }
 
   while (still_running) {
-    CURLMcode mc = curl_multi_perform(multi_handle, &still_running);
+    mc = curl_multi_perform(multi_handle, &still_running);
 
     if (still_running)
       /* wait for activity, timeout or "nothing" */
       mc = curl_multi_poll(multi_handle, NULL, 0, 1000, NULL);
 
-    if (mc)
+    if (mc != CURLM_OK) {
+      fprintf(stderr, "cannot poll handle: %s", curl_multi_strerror(mc));
       break;
+    }
   }
   /* See how the transfers went */
   while ((msg = curl_multi_info_read(multi_handle, &msgs_left))) {
@@ -96,11 +117,17 @@ main(void)
 
   /* remove the transfers and cleanup the handles */
   for (i = 0; i < HANDLECOUNT; i++) {
-    curl_multi_remove_handle(multi_handle, handles[i]);
+    mc = curl_multi_remove_handle(multi_handle, handles[i]);
+    if (mc != CURLM_OK) {
+      fprintf(stderr, "cannot remove handle: %s", curl_multi_strerror(mc));
+    }
     curl_easy_cleanup(handles[i]);
   }
 
-  curl_multi_cleanup(multi_handle);
+  mc = curl_multi_cleanup(multi_handle);
+  if (mc != CURLM_OK) {
+    fprintf(stderr, "cannot remove handle: %s", curl_multi_strerror(mc));
+  }
 
   return 0;
 }
